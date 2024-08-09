e2e506fddf37dacc046e0f40764a17a6db04db47
Itachi-Uchiha581 <adeebfaiyaz@gmail.com>
2024-08-09 14:24:12 +0530

    Tweaked the prompt

--- routeagent/prompts.py
+++ routeagent/prompts.py
@@ -15,6 +15,6 @@ class RouteAgentPrompts:
         """
     )
     user_prompt: str = (
-        "Prompt: {prompt} \n{agent_data} \nRemember you must only output a number which corresponds to an agent "
+        "Prompt: {prompt}\n{agent_data} \nRemember you must only output a number which corresponds to an agent "
         "given above based on your understanding of the prompt"
     )
